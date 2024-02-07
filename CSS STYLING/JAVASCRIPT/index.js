f3a9099ab76dbcd750788e3432727b3d7cf5b618
KHIZERBUTT07 <khizerbutt105@gmail.com>
2024-02-07 11:05:58 +0500

    javascript

---
+++ CSS STYLING/JAVASCRIPT/index.js
@@ -0,0 +1,6 @@
+// alert(document.getElementById("seven").innerText);
+
+document.getElementsByClassName("number");
+var numbers = document.getElementsByClassName('number');
+
+console.log(numbers);
