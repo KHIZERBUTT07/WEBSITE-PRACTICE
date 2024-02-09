670d5a74bad9498708f4a03b06384e952b5c353b
KHIZERBUTT07 <khizerbutt105@gmail.com>
2024-02-09 10:59:38 +0500

    updated

---
+++ CSS STYLING/JAVASCRIPT/CLASS 1/index.js
@@ -0,0 +1,38 @@
+// alert(document.getElementById("seven").innerText);
+
+document.getElementsByClassName("number");
+var numbers = document.getElementsByClassName('number');
+
+console.log(numbers);
+
+ /*var studentAge= 20+10-20+(10*2);
+console.log(studentAge);*/
+
+var count=1;
+var countDiv = document.getElementsByClassName('count');
+console.log(countDiv)
+countDiv
+
+
+
+// RE ASSIGN
+
+/*var count=10;
+count=count+1;
+console.log(count); */
+
+
+
+ /* var _name = 'khizer'
+var fatherName = 'butt'
+var space = ' '
+var fulllName = _name + ' ' + fatherName;
+console.log(fullName) */
+
+
+
+// Prompt
+
+// var username = prompt(whatIsYourUserName)
+
+
--- CSS STYLING/JAVASCRIPT/index.js
+++
@@ -1,6 +0,0 @@
-// alert(document.getElementById("seven").innerText);
-
-document.getElementsByClassName("number");
-var numbers = document.getElementsByClassName('number');
-
-console.log(numbers);
