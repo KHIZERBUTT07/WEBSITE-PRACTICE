91a9ceb6f3f743956166eaa63a480657a974cb71
KHIZERBUTT07 <khizerbutt105@gmail.com>
2024-02-26 02:41:01 +0500

    assign

---
+++ CSS STYLING/JAVASCRIPT/JS ASSIGNS/assign(2).js
@@ -0,0 +1,25 @@
+// Prompt user for age:
+var age = prompt("Enter Your Age:");
+
+// Prompt user for student status:
+var isStudent = prompt("Are You A Student?(Yes OR No)");
+
+// Initialize ticketprice variable:
+var ticketprice;
+
+// Check age and student status to determine ticket price:
+if(age < 12){
+    ticketprice = 5;
+}
+else if(age >=12 && age <=18){
+    if(isStudent === "yes"){
+        ticketprice = 8;
+    }
+    else{
+        ticketprice = 10;
+    }
+}
+else{
+    ticketprice = 12;
+}
+alert("The Ticket Price Is $" + ticketprice);
