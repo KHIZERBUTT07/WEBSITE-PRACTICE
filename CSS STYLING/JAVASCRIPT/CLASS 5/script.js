8906dad0bb3e9bfe2c3b7797bcd566a739a63ab3
KHIZERBUTT07 <khizerbutt105@gmail.com>
2024-02-29 21:17:43 +0500

    updated

---
+++ CSS STYLING/JAVASCRIPT/CLASS 5/script.js
@@ -0,0 +1,25 @@
+// for loop
+/* 1.Initialization
+   2.Condition
+   3.Satatement
+   4.Updation 
+   */
+
+// for(var i = 0; i<=10; i++){
+//       console.log('i');
+//       i++
+// }
+
+// Star Pattern
+/* **********
+*********
+********
+*******
+******  */
+    var star = "**********"
+    for(var i = 10; i >=1; i--){
+
+        console.log(star)
+        star = star.slice(1);
+        //  console.log(i);
+    }
