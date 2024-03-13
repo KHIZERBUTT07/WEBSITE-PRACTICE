f2944a8cc6412a5f739a49c024df041468887ee7
KHIZERBUTT07 <khizerbutt105@gmail.com>
2024-03-13 08:25:34 +0500

    Create index.html

---
+++ CSS STYLING/JAVASCRIPT/CLASS 10/script.js
@@ -0,0 +1,28 @@
+// SPLIT: to convert string into array 
+// JOIN: to convert array into string
+
+
+// SPLIT
+var str = "i'm a student";
+var arr = str.split(" ");
+console.log(arr)
+
+//JOIN
+var arr = ["first", "second", "third"];
+var str = arr.join(" ");
+console.log(str)
+
+
+
+
+
+// Question
+// var para = prompt("ENTER A PARAGRAPH");
+
+// var paraArray = para.split(" ")
+
+// console.log(paraArray)
+// console.log(paraArray.length);
+
+
+
