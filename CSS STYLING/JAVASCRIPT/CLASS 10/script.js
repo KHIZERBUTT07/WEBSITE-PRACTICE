17b38e67f6f54abe639550b6fd656d77e2fa4016
KHIZERBUTT07 <khizerbutt105@gmail.com>
2024-03-20 06:16:45 +0500

    Update script.js

--- CSS STYLING/JAVASCRIPT/CLASS 10/script.js
+++ CSS STYLING/JAVASCRIPT/CLASS 10/script.js
@@ -3,26 +3,46 @@
 
 
 // SPLIT
-var str = "i'm a student"; 
-var arr = str.split(" ");
-console.log(arr)
+// var str = "i'm a student"; 
+// var arr = str.split(" ");
+// console.log(arr)
 
 //JOIN
-var arr = ["first", "second", "third"];
-var str = arr.join(" "); 
-console.log(str)
+// var arr = ["first", "second", "third"];
+// var str = arr.join(" "); 
+// console.log(str)
 
 
 
 
 
 // Question
-var para = prompt("ENTER A PARAGRAPH");
+// var para = prompt("ENTER A PARAGRAPH");
 
-var paraArray = para.split(" ")
+// var paraArray = para.split(" ")
 
-console.log(paraArray)
-console.log(paraArray.length);
+// console.log(paraArray)
+// console.log(paraArray.length);
 
 
 
+// var cities = ["karachi",'islmamabad']
+// for(let i = 0; i<=cities.lenght ;i++){
+//    console.log(cities) 
+// }
+
+
+var cities = ["karachi",'islamabad',"Quetta","Sialkot","Peshawar"];
+var userInput = prompt("Enter city Name");
+var isFound = false
+
+for(var i=0; i<=cities.length; i++){
+    if(userInput===cities[i]){
+        console.log('city found :-' +  userInput);
+        isFound = true ;
+    }
+};
+
+if(isFound === false){
+  console.log('Not Found');
+}
