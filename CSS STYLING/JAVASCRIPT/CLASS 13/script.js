3f55f507f62c2b13da0b20634996421f1c6ee490
KHIZERBUTT07 <khizerbutt105@gmail.com>
2024-04-24 10:54:25 +0500

    updated

---
+++ CSS STYLING/JAVASCRIPT/CLASS 13/script.js
@@ -0,0 +1,32 @@
+// ROUND NUMBERS 
+
+var roundInput = document.getElementById('round_input');
+var roundResult = document.getElementById('round_result');
+
+
+
+// function round(){
+    
+//    var roundrzlt =  Math.round(roundInput.value);
+//     roundResult.innerText = "=>" + roundrzlt 
+// }
+
+// function ceil(){
+    
+//     var roundrzlt =  Math.ceil(roundInput.value);
+//      roundResult.innerText = "=>" + roundrzlt 
+//  }
+
+// function floor(){
+    
+//     var roundrzlt =  Math.floor(roundInput.value);
+//      roundResult.innerText = "=>" + roundrzlt 
+//  }
+
+
+// function generateRandomNumber(){
+    
+//     var roundrzlt =  Math.random(roundInput.value);
+//     var Randomto100 = Math.ceil(random * 10)
+//      roundResult.innerText = "=>" + roundrzlt + "Multiply by 100 or 10" + Randomto100
+//  } 
