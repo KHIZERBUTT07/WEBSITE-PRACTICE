c729523b655d9c9e8c1103b9cb928da48b0b2a2c
KHIZERBUTT07 <khizerbutt105@gmail.com>
2024-04-26 10:46:27 +0500

    commit

--- CSS STYLING/JAVASCRIPT/CLASS 14/script.js
+++ CSS STYLING/JAVASCRIPT/CLASS 14/script.js
@@ -1,11 +1,11 @@
 
-var result_num = document.getElementById('rsult_num');
+var result_num = document.getElementById('result_num');
 var user_num = document.getElementById('user_num');
 var result_box = document.getElementById('result_box');
 
 function checkNumber(usernumber){
 
-    var RandomNumber = math.ceil(math.random() * 6)
+    var RandomNumber = Math.ceil(Math.random() * 6)
     user_num.innerTextc = usernumber
     result_num.innerText = RandomNumber
     if(usernumber == RandomNumber){
