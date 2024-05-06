dee6b3aa695d32fb909592a1e40cc08185c599c7
KHIZERBUTT07 <khizerbutt105@gmail.com>
2024-05-06 11:00:02 +0500

    calculate

---
+++ CSS STYLING/JAVASCRIPT/CLASS 17/script.js
@@ -0,0 +1,26 @@
+
+
+   let num1 = document.getElementById('num1');
+   let num2 = document.getElementById('num2');
+   let calculation_result = document.getElementById('calculation_result')
+
+   function calculate(sign = '+'){
+    let num1Value = num1.value
+    let num2Value = num2.value
+    if(sign == '+'){
+        calculation_result.innerText = parseInt(num1Value) + parseInt(num2Value)
+    }
+    if(sign == '-'){
+        calculation_result.innerText = parseInt(num1Value) - parseInt(num2Value)
+    }
+    if(sign == '/'){
+        calculation_result.innerText = parseInt(num1Value) / parseInt(num2Value)
+    }
+    if(sign == 'x'){
+        calculation_result.innerText = parseInt(num1Value) * parseInt(num2Value)
+    }
+    // if(sign== '%'){
+    //     calculation_result.innerText = parseInt(num1Value) + parseInt(num1Value)
+    // }
+
+   }
