1c5bda6fcf9ca89487bec1c430a2d72f11d4f549
KHIZERBUTT07 <khizerbutt105@gmail.com>
2024-05-14 02:37:09 +0500

    up

---
+++ CSS STYLING/JAVASCRIPT/CLASS 19/script.js
@@ -0,0 +1,6 @@
+var div_element = document.getElementById("box1")
+var cow_element = document.getElementById("box2")
+
+div_element.addEventListener("onmouseover",function change(){
+    cow_element.src = "https://media.istockphoto.com/id/1443328418/photo/tabby-cat-closeup-portrait.webp?b=1&s=170667a&w=0&k=20&c=ZPpQHOnRDGqB1XsuCb8aIp4_mxxuBwhhZfAhCN91S1w="
+});
