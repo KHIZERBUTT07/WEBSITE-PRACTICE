a4485337fd004ff947179ebefc7e31df803b3751
KHIZERBUTT07 <khizerbutt105@gmail.com>
2024-05-18 04:29:38 +0500

    chng

---
+++ CSS STYLING/JAVASCRIPT/CLASS 20/script.js
@@ -0,0 +1,20 @@
+// var box1 = document.getElementById("box1");
+// var box2 = document.getElementById('box2');
+// var box3 = document.getElementsById('box3');
+// var box4 = document.getElementsById('box4');
+// var heading = document.getElementsById('heading');
+
+
+
+// box1.addEventListener("click",function(){
+    
+   
+//         heading.style.fontWeight = "bold";
+//     // heading.classList.toggle("bold")
+// });
+
+function makeBold(){
+    var text = document.getElementById('myText');
+
+    text.syle.fontWeight = "bold";
+}
---
+++ CSS STYLING/JAVASCRIPT/JS ASSIGNS/change.js
@@ -0,0 +1,29 @@
+function makeBold(){
+    var text = document.getElementById("myText");
+    text.style.fontWeight = "bold";
+}
+function makeItalic(){
+    var text = document.getElementById("myText");
+    text.style.fontStyle = "italic";
+}
+function makeUnderline(){
+    var text = document.getElementById("myText");
+    text.style.textDecoration = "underline";
+}
+// function makeH4(){
+//     var text = document.getElementById("myText");
+//     text.style.fontSize = "20px";
+// }
+
+function makeCap(){
+    var text = document.getElementById("myText");
+    text.style.textTransform = "uppercase"
+}
+function makeSmall(){
+    var text = document.getElementById("myText");
+    text.style.textTransform = "lowercase"
+}
+function makeSansserif(){
+    var text = document.getElementById("myText");
+    text.style.fontFamily= "sans-serif"
+}
